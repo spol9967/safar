4e0a53ce65434a7cc9ae8ac188aa8b997ee36735
sayalissalunkhe2496 <sayalissalunkhe2496@gmail.com>
2020-07-11 13:45:13 +0530

    resolved

--- safar_tours/src/components/Footer.js
+++ safar_tours/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="bg-dark py-5">
+        <div className="">
             <h1>Footer</h1>
         </div>
     )
