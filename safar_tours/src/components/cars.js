ef1950e2df288f18c6e2dd3cf3688f4e6e12ea6d
shubham.pol <shubham.pol@zeuslearning.com>
2020-07-17 22:13:05 +0530

    Bookcar.js firebase data setup

---
+++ safar_tours/src/components/cars.js
@@ -0,0 +1,78 @@
+const carData = [
+    {
+        id:1,
+        date : "Thursday 12 Dec 2019 - 15:10",
+        carImage : "/images/Zest.png",
+        carName : "Tata Zest",
+        carYearModel: 2020,
+        seatIcon : "/images/overturned-vehicle.png",
+        seats : 7,
+        facilityIcon : "/images/lines.png",
+        kilpMeterIcon : "/images/lines.png",
+        ratePerKiloMeter : "16₹/km",
+        facility :{
+            ac : "Air conditioning",
+            window : "Electric Window",
+            music : "Stereo",
+            airBags : "Air Bags",
+            locking : "Central Locking"
+        }
+    },
+    {
+        id:2,
+        date : "Thursday 12 Dec 2019 - 15:10",
+        carImage : "/images/Zest.png",
+        carName : "Tata Zest",
+        carYearModel: 2020,
+        seatIcon : "/images/overturned-vehicle.png",
+        seats : 7,
+        facilityIcon : "/images/lines.png",
+        kilpMeterIcon : "/images/lines.png",
+        ratePerKiloMeter : "16₹/km",
+        facility :{
+            ac : "Air conditioning",
+            window : "Electric Window",
+            music : "Stereo",
+            airBags : "Air Bags",
+            locking : "Central Locking"
+        }
+    },
+    {
+        id:3,
+        date : "Thursday 12 Dec 2019 - 15:10",
+        carImage : "/images/Zest.png",
+        carName : "Tata Zest",
+        carYearModel: 2020,
+        seatIcon : "/images/overturned-vehicle.png",
+        seats : 7,
+        facilityIcon : "/images/lines.png",
+        kilpMeterIcon : "/images/lines.png",
+        ratePerKiloMeter : "16₹/km",
+        facility :{
+            ac : "Air conditioning",
+            window : "Electric Window",
+            music : "Stereo",
+            airBags : "Air Bags",
+            locking : "Central Locking"
+        }
+    },
+    {
+        id:4,
+        date : "Thursday 12 Dec 2019 - 15:10",
+        carImage : "/images/Zest.png",
+        carName : "Tata Zest",
+        carYearModel: 2020,
+        seatIcon : "/images/overturned-vehicle.png",
+        seats : 5,
+        facilityIcon : "/images/lines.png",
+        kilpMeterIcon : "/images/lines.png",
+        ratePerKiloMeter : "16₹/km",
+        facility :{
+            ac : "Air conditioning",
+            window : "Electric Window",
+            music : "Stereo",
+            airBags : "Air Bags",
+            locking : "Central Locking"
+        }
+    }
+]
