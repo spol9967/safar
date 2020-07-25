292383f900b8c7c11e6a2597a48391fb6986cc96
diptirsindkar <diprsindkar11@gmail.com>
2020-07-25 09:35:47 +0530

    counter

--- safar_tours/src/components/Counts.js
+++ safar_tours/src/components/Counts.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { CountUp } from 'use-count-up';
+
+
+
+
 
 const Counts = () => {
     return (
-        <div>
-            
+        <div className="container">
+        <div className="row">
+         <div className="col-sm-3">
+         <div className="numbs">
+         <div className="value"><CountUp isCounting end={120859} duration={3.2} thousandsSeparator="," /></div>
+         <p>HAPPY TRAVELLERS</p>
+         </div>
+         </div>
+
+         <div className="col-sm-3">
+         <div className="numbs">
+         <div className="value"><CountUp isCounting end={2594} duration={3.2} thousandsSeparator="," /></div>
+         <p>SATISFIED TOURS</p>
+         </div>
+         </div>
+
+         <div className="col-sm-3">
+         <div className="numbs">
+         <div className="value"><CountUp isCounting end={854} duration={3.2} thousandsSeparator="," /></div>
+         <p>DESTINATION</p>
+         </div>
+         </div>
+
+         <div className="col-sm-3">
+         <div className="numbs">
+         <div className="value"><CountUp isCounting end={978} duration={3.2} thousandsSeparator="," /></div>
+         <p>HOTELS RESORT</p>
+         </div>
+         </div>
+        </div>
         </div>
     )
 }
