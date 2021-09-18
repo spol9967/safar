fc43bfcd6c10258d3c9af0eb68401c603bb05efd
spol9967 <spol421@gmail.com>
2021-09-18 13:35:35 +0530

    error solved: new build

---
+++ public/precache-manifest.cad41864d468218e98eb2c11533e89e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c2333d1fd60e6b9e1c786c1ed4481e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcd4bb4f00f07f69ebc",
+    "url": "/static/css/2.1bb45791.chunk.css"
+  },
+  {
+    "revision": "7005e4134775fadcc3e7",
+    "url": "/static/css/main.11f1fe62.chunk.css"
+  },
+  {
+    "revision": "abcd4bb4f00f07f69ebc",
+    "url": "/static/js/2.5aaf6c65.chunk.js"
+  },
+  {
+    "revision": "c1f712b46f543740d56312bad58b763a",
+    "url": "/static/js/2.5aaf6c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7005e4134775fadcc3e7",
+    "url": "/static/js/main.b0648308.chunk.js"
+  },
+  {
+    "revision": "7b1c1ee8e73a76dd7c47",
+    "url": "/static/js/runtime-main.7f61a39c.js"
+  }
+]);
